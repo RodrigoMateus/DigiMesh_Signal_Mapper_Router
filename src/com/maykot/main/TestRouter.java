264bfd78b50461b1b6716a3e2cb624752f9b4e78
RodrigoMateus <rodrigo.m.mateus@gmail.com>
2016-01-07 12:19:41 -0200

    nada

--- src/com/maykot/main/TestRouter.java
+++ src/com/maykot/main/TestRouter.java
@@ -15,7 +15,7 @@ import com.digi.xbee.api.exceptions.XBeeException;
 import com.maykot.radiolibrary.ErrorMessage;
 import com.maykot.radiolibrary.MessageParameter;
 import com.maykot.radiolibrary.ProxyRequest;
-import com.maykot.radiolibrary.Router;
+import com.maykot.radiolibrary.RouterRadio;
 
 public class TestRouter {
 
@@ -35,7 +35,7 @@ public class TestRouter {
 		case 1:
 			try {
 				byte[] dataToSend = new String("Hello!!!").getBytes();
-				Router.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice, MessageParameter.SEND_TXT_FILE,
+				RouterRadio.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice, MessageParameter.SEND_TXT_FILE,
 						dataToSend);
 			} catch (TimeoutException e) {
 				System.out.println(
@@ -66,7 +66,7 @@ public class TestRouter {
 				try {
 					byte[] dataToSend = Files.readAllBytes(Paths.get("Texto_Longo.txt"));
 					System.out.println("Enviando Texto Longo " + (i + 1));
-					Router.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice,
+					RouterRadio.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice,
 							MessageParameter.SEND_TXT_FILE, dataToSend);
 					Thread.sleep(3000);
 				} catch (InterruptedException e) {
@@ -101,7 +101,7 @@ public class TestRouter {
 				"As diversas finalidades do trabalho acadêmico podem se resumir em apresentar, demonstrar, difundir, recuperar ou contestar o conhecimento produzido, acumulado ou transmitido. Ao apresentar resultados, o texto acadêmico atende à necessidade de publicidade relativa ao processo de conhecimento. A pesquisa realizada, a ideia concebida ou a dedução feita perecem se não vierem a público; por esse motivo existem diversos canais de publicidade adequados aos diferentes trabalhos: as defesas públicas, os periódicos, as comunicações e a multimídia virtual são alguns desses. A demonstração do conhecimento é necessidade na comunidade acadêmica, onde esse conhecimento é o critério de mérito e acesso. Assim, existem as provas, concursos e diversos outros processos de avaliação pelos quais se constata a construção ou transmissão do saber. Difundir o conhecimento às esferas externas à comunidade acadêmica é atividade cada vez mais presente nas instituições de ensino, pesquisa e extensão, e o texto correspondente a essa prática tem característica própria sem abandonar a maior parte dos critérios de cientificidade. A recuperação do conhecimento é outra finalidade do texto acadêmico. Com bastante freqüência, parcelas significativas do conhecimento caem no esquecimento das comunidades e das pessoas; a recuperação e manutenção ativa da maior diversidade de saberes é finalidade importante de atividades científicas objeto da produção de texto. Quase todo conhecimento produzido é contestado. Essa contestação, em que não constitua conhecimento diferenciado, certamente é etapa contribuinte no processo da construção do saber que contesta, quer por validá-lo, quer por refutá-lo. As finalidades do texto acadêmico certamente não se esgotam nessas, mas ficam aqui exemplificadas. Para atender à diversidade dessas finalidades, existe a multiplicidade de formas, entre as quais se encontram alguns conhecidos tipos, sobre os quais se estabelece conceito difuso.")
 						.getBytes();
 		try {
-			Router.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice, MessageParameter.SEND_TXT_FILE,
+			RouterRadio.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice, MessageParameter.SEND_TXT_FILE,
 					dataToSend);
 		} catch (TimeoutException e) {
 			System.out.println(
@@ -138,7 +138,7 @@ public class TestRouter {
 		byte[] dataToSend = SerializationUtils.serialize(proxyRequest);
 
 		try {
-			Router.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice, MessageParameter.SEND_HTTP_POST,
+			RouterRadio.getInstance().sendMessage(MainApp.myDevice, MainApp.remoteDevice, MessageParameter.SEND_HTTP_POST,
 					dataToSend);
 		} catch (TimeoutException e) {
 			System.out.println(
