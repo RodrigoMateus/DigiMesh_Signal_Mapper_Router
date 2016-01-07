16299b79178257c5ef8eda49bf4f6f0e89eb6d5d
RodrigoMateus <rodrigo.m.mateus@gmail.com>
2016-01-07 12:21:28 -0200

    OpenDevice() e DiscoverRemoteDevice() migraram para RadioJavaLibrary()

--- src/com/maykot/main/MainApp.java
+++ src/com/maykot/main/MainApp.java
@@ -2,26 +2,34 @@ package com.maykot.main;
 
 import java.io.IOException;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
 import com.digi.xbee.api.DigiMeshDevice;
 import com.digi.xbee.api.RemoteXBeeDevice;
-import com.digi.xbee.api.XBeeNetwork;
 import com.digi.xbee.api.exceptions.XBeeException;
-import com.digi.xbee.api.models.APIOutputMode;
 import com.digi.xbee.api.utils.DeviceConfig;
 import com.digi.xbee.api.utils.LogRecord;
-import com.digi.xbee.api.utils.SerialPorts;
-import com.maykot.radiolibrary.Router;
+import com.maykot.mqtt.RouterMqtt;
+import com.maykot.radiolibrary.DiscoverRemoteDevice;
+import com.maykot.radiolibrary.OpenMyDevice;
+import com.maykot.radiolibrary.RouterRadio;
 
 public class MainApp {
+
+	/* Arquivo de configurações do sistema */
+	static DeviceConfig deviceConfig;
 
 	/* XTends */
 	public static DigiMeshDevice myDevice;
 	public static RemoteXBeeDevice remoteDevice;
-	public static DeviceConfig deviceConfig;
-	static String XTEND_PORT = null;
-	static int XTEND_BAUD_RATE;
-	static int TIMEOUT_FOR_SYNC_OPERATIONS = 10000; // 10 seconds
-	static String REMOTE_NODE_IDENTIFIER = null;
+
+	/* MQTT */
+	static String BROKER_URL = null;
+	static String CLIENT_ID = null;
+	static String SUBSCRIBED_TOPIC = null;
+	static int QoS = -1;
+	static MqttClient mqttClient;
 
 	public static void main(String[] args) {
 		System.out.println(" +-------------------+");
@@ -30,81 +38,42 @@ public class MainApp {
 
 		try {
 			deviceConfig = new DeviceConfig();
-			XTEND_PORT = deviceConfig.getXTendPort();
-			XTEND_BAUD_RATE = deviceConfig.getXTendBaudRate();
-			REMOTE_NODE_IDENTIFIER = deviceConfig.getRemoteNodeID();
+
+			BROKER_URL = deviceConfig.getBrokerURL();
+			CLIENT_ID = deviceConfig.getClientId();
+			SUBSCRIBED_TOPIC = deviceConfig.getSubscribedTopic();
+			QoS = deviceConfig.getQoS();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-
 		new LogRecord();
 
-		openMyDevice();
+		myDevice = OpenMyDevice.open(deviceConfig);
 
 		// Registra listener para processar mensagens recebidas
-		Router.getInstance().processMyMessage(new ProcessMessage());
+		RouterRadio.getInstance().processMyMessage(new ProcessMessage());
 
 		try {
-			discoverDevice();
-		} catch (XBeeException e1) {
+			remoteDevice = DiscoverRemoteDevice.discover(deviceConfig, myDevice);
+		} catch (IOException e2) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e2.printStackTrace();
+		} catch (XBeeException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+
+		try {
+			mqttClient = new MqttClient(BROKER_URL, CLIENT_ID, null);
+			mqttClient.setCallback(new RouterMqtt());
+			mqttClient.connect();
+			mqttClient.subscribe(SUBSCRIBED_TOPIC, QoS);
+		} catch (MqttException e) {
+			e.printStackTrace();
 		}
 
 		// Menu para seleção de Testes
 		TestRouter.showMenu();
-	}
-
-	public static void openMyDevice() {
-		try {
-			XTEND_PORT = deviceConfig.getXTendPort();
-			myDevice = openDevice(XTEND_PORT, XTEND_BAUD_RATE);
-			myDevice.setReceiveTimeout(TIMEOUT_FOR_SYNC_OPERATIONS);
-			myDevice.addExplicitDataListener(Router.getInstance());
-			System.out.println("Was found LOCAL radio " + myDevice.getNodeID() + " (PowerLevel "
-					+ myDevice.getPowerLevel() + ").");
-			return;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for (String port : SerialPorts.getSerialPortList()) {
-			try {
-				System.out.println("Try " + port);
-				myDevice = openDevice(port, XTEND_BAUD_RATE);
-				myDevice.addExplicitDataListener(Router.getInstance());
-				System.out.println("Was found LOCAL radio " + myDevice.getNodeID() + " (PowerLevel: "
-						+ myDevice.getPowerLevel() + ").");
-				return;
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println("openDevice() ERROR");
-			}
-		}
-		System.out.println("LOCAL Radio not found! Try openDevice() again.");
-		openMyDevice();
-	}
-
-	public static DigiMeshDevice openDevice(String port, int bd) throws Exception {
-		DigiMeshDevice device = new DigiMeshDevice(port, bd);
-		device.open();
-		device.setAPIOutputMode(APIOutputMode.MODE_EXPLICIT);
-		return device;
-	}
-
-	public static void discoverDevice() throws XBeeException {
-		// Obtain the remote XBee device from the XBee network.
-		XBeeNetwork xbeeNetwork = myDevice.getNetwork();
-
-		do {
-			remoteDevice = xbeeNetwork.discoverDevice(REMOTE_NODE_IDENTIFIER);
-			if (remoteDevice == null) {
-				System.out.println("Couldn't find the Radio " + REMOTE_NODE_IDENTIFIER + ".");
-			}
-		} while (remoteDevice == null);
-
-		System.out.println("Was found REMOTE radio " + REMOTE_NODE_IDENTIFIER + " (PowerLevel "
-				+ remoteDevice.getPowerLevel() + ").");
 	}
 
 }
